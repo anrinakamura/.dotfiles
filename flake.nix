fb271fa52c42f3a94a480adc00c912374cc51251
anrinakamura <an12inakamu12a@gmail.com>
2025-06-19 08:33:19 +0900

    update

--- flake.nix
+++ flake.nix
@@ -5,75 +5,78 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager"; 
-      inputs.nixpkgs.follows = "nixpkgs"; 
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { 
-    self, 
-    nixpkgs, 
-    home-manager, 
-    ... 
-  } @ inputs: let 
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    } @ inputs:
+    let
       systems = [
         "x86_64-linux"
-	# "aarch64-darwin"
-      ]; 
-      forAllSystems = nixpkgs.lib.genAttrs systems; 
+        # "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
       apps = forAllSystems (
-        system: 
-	let 
-	  pkgs = nixpkgs.legacyPackages.${system}; 
-	in
-	{
-	  # `nix run .`
-	  default = {
-            type = "app"; 
-	    program =  toString (
-	      pkgs.writeShellScript "default" ''
-	        set -e 
-		uname -a
-	      '');
-	  }; 
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          # `nix run .`
+          default = {
+            type = "app";
+            program = toString (
+              pkgs.writeShellScript "default" ''
+                	        set -e 
+                		uname -a
+                	      ''
+            );
+          };
 
-	  # `nix run .#update`
-	  update = {
-            type = "app"; 
-	    program = toString (
-	      pkgs.writeShellScript "update" ''
-	        set -e 
-		echo "updating flake..."
-                nix flake update
-		echo "updates successfully completed!"
-	      ''); 
-	  }; 
-	} 
+          # `nix run .#update`
+          update = {
+            type = "app";
+            program = toString (
+              pkgs.writeShellScript "update" ''
+                	        set -e 
+                		echo "updating flake..."
+                                nix flake update
+                		echo "updates successfully completed!"
+                	      ''
+            );
+          };
+        }
       );
 
       # `nix fmt`
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style); 
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
 
-      # `USER="xxx" HOME="xxx" home-manager --impure --flake .#default`
+      # `USER="xxx" HOME="xxx" home-manager switch --impure --flake .#default`
       homeConfigurations = forAllSystems (
         system:
-        let 
-	  pkgs = nixpkgs.legacyPackages.${system}; 
-	in 
-	{
-	  default = home-manager.lib.homeManagerConfiguration {
-	    inherit pkgs; 
-	    extraSpecialArgs = {
-	      inherit inputs;
-	      pkgs = pkgs; 
-	    }; 
-	    modules = [ ./home.nix ];
-	  }; 
-	}
-      ); 
-      
-    }; 
-}
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            extraSpecialArgs = {
+              inherit inputs;
+              pkgs = pkgs;
+            };
+            modules = [ ./home.nix ];
+          };
+        }
+      );
 
+    };
+}
--- home.nix
+++ home.nix
@@ -1,21 +1,20 @@
-{ inputs, ... }: 
+{ inputs, pkgs, ... }:
 
 let
   username = builtins.getEnv "USER";
   homeDirectory = builtins.getEnv "HOME";
 
-  # gitUsername = builtins.getEnv "GIT_USERNAME"; 
+  # gitUsername = builtins.getEnv "GIT_USERNAME";
   # gitEmail = builtins.getEnv "GIT_EMAIL";
 in
 {
   home = {
-    inherit username homeDirectory; 
+    inherit username homeDirectory;
   };
 
   home.packages = with pkgs; [
     neofetch
-  ]; 
+  ];
 
-  programs.home-manager.enable = true; 
+  programs.home-manager.enable = true;
 }
-
