62998871933cd127a4bf01abed34fede42b97347
anrinakamura <89412831+anrinakamura@users.noreply.github.com>
2025-06-19 09:01:03 +0900

    feat: introduce home-manager. (#1)
    * wip
    
    * update
    
    * update
    
    * update
    
    * fix
    
    * update

--- flake.nix
+++ flake.nix
@@ -3,62 +3,81 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = 
-    { self, nixpkgs, ... }: 
-    let 
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
       systems = [
         "x86_64-linux"
-	# "aarch64-darwin"
-      ]; 
-      forAllSystems = nixpkgs.lib.genAttrs systems; 
+        # "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
-      apps = forAllSystems(
-        system: 
-	let 
-	  pkgs = nixpkgs.legacyPackages.${system}; 
-	in
-	{
-	  # `nix run .`
-	  default = {
-            type = "app"; 
-	    program =  toString (
-	      pkgs.writeShellScript "default" ''
-	        set -e 
-		uname -a
-	      '');
-	  }; 
+      apps = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          # `nix run .`
+          default = {
+            type = "app";
+            program = toString (
+              pkgs.writeShellScript "default" ''
+                	        set -e 
+                		uname -a
+                	      ''
+            );
+          };
 
-	  # `nix run .#update`
-	  update = {
-            type = "app"; 
-	    program = toString (
-	      pkgs.writeShellScript "update" ''
-	        set -e 
-		echo "updating flake..."
-                nix flake update
-		echo "updates successfully completed!"
-	      ''); 
-	  }; 
-	} 
+          # `nix run .#update`
+          update = {
+            type = "app";
+            program = toString (
+              pkgs.writeShellScript "update" ''
+                                	        set -e 
+                                		echo "updating flake..."
+                                                nix flake update
+                				echo "updating home-manager..."
+                				nix run nixpkgs#home-manager -- switch --flake .#default --show-trace --impure
+                                		echo "updates successfully completed!"
+                                	      ''
+            );
+          };
+        }
       );
 
-      packages = forAllSystems (
-        system: 
-	let
-	  pkgs = nixpkgs.legacyPackages.${system}; 
-	in
-	{
-	  neofetch = pkgs.neofetch; 
+      # `nix fmt`
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
 
-	  # `nix fmt`
-	  formatter = pkgs.nixfmt-rfc-style; 
+      # `USER="xxx" HOME="xxx" home-manager switch --flake .#default --impure`
+      #  or `nix run nixpkgs#home-manager -- switch --flake .#default --impure`
+      homeConfigurations =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        {
+          default = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            extraSpecialArgs = {
+              inherit inputs;
+              pkgs = pkgs;
+            };
+            modules = [ ./home.nix ];
+          };
+        };
 
-	}
-      ); 
-
-    }; 
+    };
 }
-
---
+++ home.nix
@@ -0,0 +1,23 @@
+{ inputs, pkgs, ... }:
+
+let
+  username = builtins.getEnv "USER";
+  homeDirectory = builtins.getEnv "HOME";
+
+  # gitUsername = builtins.getEnv "GIT_USERNAME";
+  # gitEmail = builtins.getEnv "GIT_EMAIL";
+in
+{
+  home = {
+    inherit username homeDirectory;
+
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    stateVersion = "24.05";
+  };
+
+  home.packages = with pkgs; [
+    neofetch
+  ];
+
+  programs.home-manager.enable = true;
+}
