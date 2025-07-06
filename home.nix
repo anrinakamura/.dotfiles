b5b4e30be46743c44b553bfb8d5b4675217e00aa
anrinakamura <an12inakamu12a@gmail.com>
2025-07-07 07:03:15 +0900

    update

--- home.nix
+++ home.nix
@@ -56,7 +56,7 @@ in
   programs.tmux = {
     enable = true;
     keyMode = "vi";
-    terminal = "xterm-256color"; 
+    terminal = "xterm-256color";
     extraConfig = ''
       # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
       set -g default-terminal "xterm-256color"
