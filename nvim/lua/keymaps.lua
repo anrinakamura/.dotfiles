c102cf262bb80f32da2d169f7fff4d2ded14dd6c
anrinakamura <89412831+anrinakamura@users.noreply.github.com>
2025-07-19 11:54:20 +0900

    update

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,6 +1,5 @@
 -- go to next tab
-vim.keymap.set('n', '<C-l>', ':tabnext<CR>')
+vim.keymap.set("n", "<C-l>", ":tabnext<CR>")
 
 -- go to previous tab
-vim.keymap.set('n', '<C-h>', ':tabprevious<CR>')
-
+vim.keymap.set("n", "<C-h>", ":tabprevious<CR>")
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,23 @@
 -- syntax highlight
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        highlight = { enable = true },
-        indent = { enable = true },
-        ensure_installed = { 
-            "lua", 
-            "rust", 
-            "kotlin", 
-            "java", 
-            "c", 
-            "cpp", 
-            "python", 
-            "go", 
-        }, 
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = {
+					"lua",
+					"rust",
+					"kotlin",
+					"java",
+					"c",
+					"cpp",
+					"python",
+					"go",
+				},
+			})
+		end,
+	},
 }
-
